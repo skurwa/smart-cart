3c91d3d1ff0c79582eed071abe53a98487653943
Siddharth Kurwa <skurwa@utexas.edu>
2018-11-11 23:05:36 -0600

    poster draft

---
+++ Analysis/motorAnalysis.m
@@ -0,0 +1,24 @@
+voltage     = 12;   % volts
+stallTorque = 110;  % oz-in
+noLoadSpeed = 350;  % rpm
+noLoadCur   = .3;   % mA
+stallCur    = 5;    % A
+
+
+speed  = 0:.5:noLoadSpeed;
+torque = stallTorque - (stallTorque * speed /noLoadSpeed);
+power  = (torque .* speed) / 1352.2943390876; % power in watts
+
+% plotting
+figure(1);
+hold on;
+fontsize = 24;
+[ax, T, P] = plotyy(speed, torque,speed, power);
+set(ax, 'Fontsize', fontsize);
+T.LineWidth = 5;
+P.LineWidth = 5;
+ylabel(ax(1), "Torque (oz-in)", 'fontsize', fontsize);
+ylabel(ax(2), "Output Power (W)", 'fontsize', fontsize);
+xlabel("Speed (RPM)", 'fontsize', fontsize);
+title("Torque vs Output Power, 30:1 Pololu Motor", 'fontsize', fontsize);
+
